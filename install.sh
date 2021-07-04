355cb4a8ab67ee74b692d2ad73620e75125f65e8
Keanu <keanutimmermans8@gmail.com>
2021-07-04 14:14:39 +0200

    [codespaces] Add install script.

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo hostname KeanuCodespaces
+rm -rf ~/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --recurse-submodules https://github.com/keanuplayz/dotfiles ~/.dotfiles
+echo "source ~/.dotfiles/zsh/zshrc" >> ~/.zshrc
+pip install colorama psutil distro
+echo "zsh" >> ~/.bashrc
+source ~/.bashrc
