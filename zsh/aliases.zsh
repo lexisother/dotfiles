4ad5fb7960fee4b46c5bd24c690d140e0bc94263
Alyxia Sother <alyxia@riseup.net>
2023-02-08 12:47:41 +0100

    [zsh] Fix the md2htmldoc alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias welcome="welcome --extra-logos-dir $K_ZSH_DOTFILES/../script-resources/welcome/logos"
+alias markdown2htmldoc="$K_ZSH_DOTFILES/../scripts/markdown2htmldoc"
 
 if (( _is_macos )); then
   if command_exists exa; then
