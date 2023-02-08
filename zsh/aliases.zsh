9d05d657599618e532bbd3af6b87e92fdff47a85
Alyxia Sother <alyxia@riseup.net>
2023-02-08 12:33:11 +0100

    [zsh] Fix ls on macOS

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,9 @@
 alias welcome="welcome --extra-logos-dir $K_ZSH_DOTFILES/../script-resources/welcome/logos"
+
+if (( _is_macos )); then
+  if command_exists exa; then
+    alias ls="exa --classify --group-directories-first"
+  else
+    alias ls="ls --group-directories-first --color=auto"
+  fi
+fi
