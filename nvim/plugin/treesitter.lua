f9c86ee14df5af3d468bbba38746f984e31ce721
Alyxia Sother <alyxia@riseup.net>
2023-03-14 09:55:30 +0100

    [nvim] Add a fix for Treesitter on macOS

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,6 +1,18 @@
 local ok, parser_configs = pcall(require, 'nvim-treesitter.parsers')
 if not ok then return end
 
+-- Should only really run on my Macbook, as it is my only macOS device.
+-- Apple's C compilers seem to not want to compile any of Treesitter's
+-- dialects.
+-- That is, `cc`, `gcc`, `g++`, andsoforth. Funnily enough, they are all
+-- aliases to the same program.
+-- So, the nix-darwin configuration specifies the installation of zig as a
+-- replacement compiler solely so the Treesitter dialects compile. Very
+-- intuitive.
+if vim.loop.os_uname().sysname == 'Darwin' then
+    require('nvim-treesitter.install').compilers = { "zig" }
+end
+
 local parser_configs = parser_configs.get_parser_configs()
 
 parser_configs.norg = {
