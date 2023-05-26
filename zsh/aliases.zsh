fbe6b174bda849a40eab5e7f4cbee60ad8498cb6
Alyxia Sother <alyxia@riseup.net>
2023-05-26 16:42:43 +0200

    [scripts] Vendor mediawiki-preview and fix it (sort of)

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias welcome="welcome --extra-logos-dir $K_ZSH_DOTFILES/../script-resources/welcome/logos"
 alias markdown2htmldoc="$K_ZSH_DOTFILES/../scripts/markdown2htmldoc"
+alias mediawiki-preview="$K_ZSH_DOTFILES/../scripts/mediawiki-preview"
 
 if (( _is_macos )); then
   if command_exists exa; then
