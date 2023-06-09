4ea89899caf8a0fd87cdfdc4601f7e79fc666c37
Alyxia Sother <alyxia@riseup.net>
2023-06-09 11:19:42 +0200

    [nix] ALL HAIL THE FLAKEN!!!

---
+++ nix/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "flake of the lyxer...";
+
+  inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+      home-manager.url = "github:nix-community/home-manager";
+      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+      # nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
+      darwin.url = "github:lnl7/nix-darwin";
+      darwin.inputs.nixpkgs.follows = "nixpkgs"; # ...
+  };
+  
+  # add the inputs declared above to the argument attribute set
+  outputs = { self, nixpkgs, home-manager, darwin }: {
+    darwinConfigurations."alymac" = darwin.lib.darwinSystem {
+      system = "x86_64-darwin";
+      modules = [ ./hosts/alymac/default.nix ];
+    };
+  };
+}
---
+++ nix/hosts/alymac/default.nix
@@ -0,0 +1,24 @@
+# hosts/YourHostName/default.nix
+{ pkgs, ... }:
+{
+  # Make sure the nix daemon always runs
+  services.nix-daemon.enable = true;
+  # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
+  # services.nix-daemon.package = pkgs.nixFlakes;
+  
+  # if you use zsh (the default on new macOS installations),
+  # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
+  programs.zsh.enable = true;
+  # bash is enabled by default
+
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+    };
+    casks = [
+      "discord"
+    ];
+  };
+}
