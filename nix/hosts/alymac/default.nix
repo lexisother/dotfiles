370a8881b551dd50a982a54f3a35fa99f22152a1
Alyxia Sother <alyxia@riseup.net>
2023-06-09 13:30:06 +0200

    [nix] Fix a CRITICAL BUG with the zsh config

--- nix/hosts/alymac/default.nix
+++ nix/hosts/alymac/default.nix
@@ -6,6 +6,10 @@
   # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
   # services.nix-daemon.package = pkgs.nixFlakes;
 
+  # if you use zsh (the default on new macOS installations),
+  # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
+  programs.zsh.enable = true;
+
   users.users.alyxia = {
     name = "alyxia";
     home = "/Users/alyxia";
