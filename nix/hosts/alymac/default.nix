269924cae929e5169fafa9a293cfa867a8a1cacb
Alyxia Sother <alyxia@riseup.net>
2023-06-09 12:44:49 +0200

    [nix] Fix a CRITICAL BUG in the user setup

--- nix/hosts/alymac/default.nix
+++ nix/hosts/alymac/default.nix
@@ -5,11 +5,17 @@
   services.nix-daemon.enable = true;
   # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
   # services.nix-daemon.package = pkgs.nixFlakes;
+  users.users.alyxia = {
+    name = "alyxia";
+    home = "/Users/alyxia";
+  };
   
   # if you use zsh (the default on new macOS installations),
   # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
   programs.zsh.enable = true;
   # bash is enabled by default
+  # enable the gpg agent by default
+  programs.gnupg.agent.enable = true;
 
   homebrew = {
     enable = true;
