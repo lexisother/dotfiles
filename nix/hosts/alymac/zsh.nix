fdcefe5c7e2efe9d875529b9bbb92fa49cbfe60b
Alyxia Sother <alyxia@riseup.net>
2023-06-13 15:42:03 +0200

    [nix] Add a couple shell variables for Fy apps

--- nix/hosts/alymac/zsh.nix
+++ nix/hosts/alymac/zsh.nix
@@ -8,6 +8,8 @@
       source ${dotfiles}/zsh/zshrc
 
       export PATH="$PATH:$HOME/.npm-packages/bin"
+      export FRENYARD_SCALE=2.40
+      export RETHINK_API="https://rethink.alyxia.dev"
     '';
   };
 }
