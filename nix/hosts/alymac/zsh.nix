e7576c13c466b618ba2cd26b0ae67dd91389633b
Alyxia Sother <alyxia@riseup.net>
2023-06-20 15:38:48 +0200

    [nix] Add ~/.local/bin to PATH

--- nix/hosts/alymac/zsh.nix
+++ nix/hosts/alymac/zsh.nix
@@ -7,6 +7,9 @@
       export GPG_TTY=$(tty)
       source ${dotfiles}/zsh/zshrc
 
+      [[ -d "$HOME/.local/bin" ]] || mkdir "$HOME/.local/bin"
+
+      export PATH="$PATH:$HOME/.local/bin"
       export PATH="$PATH:$HOME/.npm-packages/bin"
       export FRENYARD_SCALE=2.40
       export RETHINK_API="https://rethink.alyxia.dev"
