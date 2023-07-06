9d46c1705780266815d8672c8cae0bc740121ad3
Alyxia Sother <alyxia@riseup.net>
2023-07-06 18:56:25 +0200

    [common/hc] emojiBlacklist: make it configurable

--- common/hellcooling/config/global.d.ts
+++ common/hellcooling/config/global.d.ts
@@ -3,5 +3,8 @@ export {};
 declare global {
   const hh: {
     constants: Record<string, string> & { categories: Record<string, string> };
+    // TODO: Maybe turn this into something that, based on the moduleName, can
+    // fetch an ext's actual config type and return that instead.
+    getModuleOptions: (moduleName: string) => Record<string, any>;
   };
 }
