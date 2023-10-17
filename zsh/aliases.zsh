b24ab449225fafb13e299a789cd3a821c230df58
Alyxia Sother <alyxia@riseup.net>
2023-10-17 10:56:31 +0200

    [zsh] Add alias for Laravel Sail

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,7 @@
 alias welcome="welcome --extra-logos-dir $K_ZSH_DOTFILES/../script-resources/welcome/logos"
 alias markdown2htmldoc="$K_ZSH_DOTFILES/../scripts/markdown2htmldoc"
 alias mediawiki-preview="$K_ZSH_DOTFILES/../scripts/mediawiki-preview"
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
 
 if (( _is_macos )); then
   if command_exists exa; then
