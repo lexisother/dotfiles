f7803324c9bcb9792c1eb1670fc8d21f3945135c
Alyxia Sother <alyxia@riseup.net>
2023-10-31 10:52:26 +0100

    [common/hc] Add hh.emitter to typedefs

--- common/hellcooling/config/global.d.ts
+++ common/hellcooling/config/global.d.ts
@@ -1,7 +1,9 @@
 export {};
+import { EventEmitter } from 'events';
 
 declare global {
   const hh: {
+    emitter: EventEmitter;
     constants: Record<string, string> & { categories: Record<string, string> };
     // TODO: Maybe turn this into something that, based on the moduleName, can
     // fetch an ext's actual config type and return that instead.
