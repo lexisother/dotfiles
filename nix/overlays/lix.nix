b1d85b15db685a47e61f06ec3c0a5d0557a684f9
Alyxia Sother <alyxia@riseup.net>
2024-07-19 16:14:57 +0200

    [nix] Disable lix tests

--- nix/overlays/lix.nix
+++ nix/overlays/lix.nix
@@ -3,6 +3,10 @@
     postPatch = (oldAttrs.postPatch or "") + ''
       substituteInPlace src/libmain/shared.cc \
         --replace-fail "(Lix, like Nix)" "but for lesbians"
-      '';
+    '';
+
+    # WARNING: This greatly assumes that lix will never merge a CL that breaks
+    # the tests. But I choose to disable them anyway because the build time is faster
+    doCheck = false;
   });
 })
