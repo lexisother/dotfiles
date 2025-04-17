37b24e287d5c216aa02e66ed26cc52d10a2e6d0c
Alyxia Sother <alyxia@riseup.net>
2025-04-17 19:40:07 +0200

    [zsh] Add a craft alias if DDEV is installed

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,12 @@ alias markdown2htmldoc="$K_ZSH_DOTFILES/../scripts/markdown2htmldoc"
 alias mediawiki-preview="$K_ZSH_DOTFILES/../scripts/mediawiki-preview"
 alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
 
+if command_exists ddev; then
+  function craft() {
+    [ -f craft ] && ddev craft $@ || return 1
+  }
+fi
+
 if (( _is_macos )); then
   if command_exists exa; then
     alias ls="exa --classify --group-directories-first"
