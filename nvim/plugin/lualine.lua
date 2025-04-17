1742107413f025dff54750d930527260bca31f64
Alyxia Sother <alyxia@riseup.net>
2025-04-17 19:39:48 +0200

    [nvim] Move coc-languages and fix lualine theme

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -3,10 +3,9 @@ local theme = require'dotfiles.colorscheme'
 
 -- https://github.com/dmitmel/dotfiles/commit/bf96e553764e3a166286f5a6a8017e01dadcf6f9
 -- https://ptb.discord.com/channels/382339402338402315/382339402338402317/885864846892077086
-function get_color(index)
+local function get_color(index)
   local color = theme.base16_colors[index]
-  local r, g, b = unpack(color, 1, 3)
-  return string.format('#%02x%02x%02x', r, g, b)
+  return color.gui
 end
 
 -- Structure of themes is as follows:
@@ -14,21 +13,21 @@ end
 -- b: secondary components, such git branch info
 -- c: Middle components, basically everything in the middle
 
-dotfiles_gruv.normal.a.bg = get_color(12)
+dotfiles_gruv.normal.a.bg = get_color(11)
 
-dotfiles_gruv.insert.a.bg = get_color(14)
-dotfiles_gruv.insert.c.bg = get_color(2)
+dotfiles_gruv.insert.a.bg = get_color(13)
+dotfiles_gruv.insert.c.bg = get_color(1)
 
-dotfiles_gruv.visual.a.bg = get_color(15)
-dotfiles_gruv.visual.c.bg = get_color(2)
-dotfiles_gruv.visual.c.fg = get_color(7)
+dotfiles_gruv.visual.a.bg = get_color(14)
+dotfiles_gruv.visual.c.bg = get_color(1)
+dotfiles_gruv.visual.c.fg = get_color(6)
 
-dotfiles_gruv.command.a.bg = get_color(16)
+dotfiles_gruv.command.a.bg = get_color(15)
 dotfiles_gruv.command.c.bg = get_color(2)
-dotfiles_gruv.command.c.fg = get_color(7)
+dotfiles_gruv.command.c.fg = get_color(6)
 
-dotfiles_gruv.replace.a.bg = get_color(9)
-dotfiles_gruv.replace.c.bg = get_color(2)
+dotfiles_gruv.replace.a.bg = get_color(8)
+dotfiles_gruv.replace.c.bg = get_color(1)
 
 -- Displays the currently selected Arduino board
 local function showBoard()
