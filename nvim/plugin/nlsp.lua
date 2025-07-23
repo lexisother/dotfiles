2728d6da3ae7fdc83de22f5751d1ab49cacaecbe
Alyxia Sother <alyxia@riseup.net>
2025-07-23 11:32:55 +0200

    [nvim] Enable vimls

--- nvim/plugin/nlsp.lua
+++ nvim/plugin/nlsp.lua
@@ -1,8 +1,7 @@
-local has_lsp, lsp = pcall(require, "vim.lsp")
-if not (has_lsp and lsp.config ~= nil and vim.g.vim_ide == 2) then
-	return
-end
+local has_lsp, lsp = pcall(require, 'vim.lsp')
+if not (has_lsp and lsp.config ~= nil and vim.g.vim_ide == 2) then return end
 
-require("dotfiles.lsp_ignition").enable({
-	"php",
+require('dotfiles.lsp_ignition').enable({
+  'php',
+  'vimls',
 })
